8cf75db395fe2e741a5b30a85df839020c0b59f7
Ratama <ratama.adhi@gmail.com>
2023-03-12 17:17:11 +0700

    feat(be): searchUser addRemoveFriend getUserFriends

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,6 +8,96 @@ export const getUser = async (req, res) => {
         delete userObj.password;
         res.status(200).json(userObj);
     } catch (error) {
-        res.status(404).json({ message: err.message });
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const searchUser = async (req, res) => {
+    try {
+        const { search: searchQuery } = req.query;
+        if (!searchQuery)
+            return res.status(404).json({ msg: "User does not found. " });
+
+        const userList = await User.find({
+            $or: [
+                {
+                    firstName: new RegExp(searchQuery, "i"),
+                },
+                {
+                    lastName: new RegExp(searchQuery, "i"),
+                },
+                {
+                    email: new RegExp(searchQuery, "i"),
+                },
+            ],
+        });
+        if (userList.length <= 0)
+            return res.status(404).json({ msg: "User does not found. " });
+
+        res.status(200).json(userList);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export function createFriendObj(userObj) {
+    return {
+        _id: userObj._id,
+        firstName: userObj.firstName,
+        lastName: userObj.lastName,
+        picturePath: userObj.picturePath,
+        occupation: userObj.occupation,
+        location: userObj.location,
+    };
+}
+
+export const addRemoveFriend = async (req, res) => {
+    try {
+        const friendId = req.params.id;
+        const userId = req.user.id;
+
+        if (!userId) return res.status(300).json("Undefined user id");
+        if (!friendId) return res.status(300).json("Undefined friend id");
+
+        const user = await User.findById(userId);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((uf) => uf !== friendId);
+            friend.friends = friend.friends.filter((ff) => ff !== userId);
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(userId);
+        }
+
+        user.save();
+        friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((ufId) => User.findById(ufId))
+        );
+
+        const formatFriendObj = friends.map((fr) => {
+            return createFriendObj(fr);
+        });
+
+        res.status(200).json(formatFriendObj);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getUserFriends = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) return res.status(300).json("Undefined user id");
+        const user = await User.findById(id);
+        const getFriends = await Promise.all(
+            user.friends.map((fId) => User.findById(fId))
+        );
+        const formatFriendObj = getFriends.map((fr) => createFriendObj(fr));
+        res.status(200).json(formatFriendObj);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getUser } from "../controllers/user.js";
+import { addRemoveFriend, getUser, getUserFriends, searchUser } from "../controllers/user.js";
 import { verifyToken } from "../midleware/auth.js";
 
 const router = express.Router();
 
+router.get("/", verifyToken, searchUser)
 router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+router.post("/addRemove/:id", verifyToken, addRemoveFriend);
 
 export default router;
